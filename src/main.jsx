e0fb90f15785e10b9116c2a3c8c0bcd1d7c56429
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-07-31 18:28:01 -0300

    chore: (App) - Adicona script de configuracao de tema

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import ReactDOM from 'react-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import App from './App';
+import theme from './styles/theme';
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </React.StrictMode>,
